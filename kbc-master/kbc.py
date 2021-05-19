e62f69014c8f3d7f668268dcd13672b47827b43b
Rishabh-Pagaria <rpagaria2000@gmail.com>
2021-05-19 05:55:55 +0530

    let's play kbc 😈

--- kbc-master/kbc.py
+++ kbc-master/kbc.py
@@ -1,7 +1,7 @@
 from questions import QUESTIONS
 
 
-def isAnswerCorrect(question, answer):
+def isAnswerCorrect(question, ans):
 
     '''
     :param question: question (Type JSON)
@@ -11,15 +11,24 @@ def isAnswerCorrect(question, answer):
         False if the answer is incorrect
     '''
 
-    return True if answer == 2 else False      #remove this
+    return True if question["answer"] == ans else False      #remove this
 
 
 def lifeLine(ques):
-
+    import random
     '''
     :param ques: The question for which the lifeline is asked for. (Type JSON)
     :return: delete the key for two incorrect options and return the new ques value. (Type JSON)
     '''
+    
+    count = 0
+    while count <= 2:
+        random_opt = random.randint(1, 4)
+        if ques["answer"] != random_opt:
+            random_opt = "option" + str(random_opt)
+            ques[random_opt] = ""
+            count += 1
+    return ques
 
 
 def kbc():
@@ -47,28 +56,60 @@ def kbc():
     #  Display a welcome message only once to the user at the start of the game.
     #  For each question, display the prize won until now and available life line.
     # For now, the below code works for only one question without LIFE-LINE and QUIT checks
+    ques_cnt = 0
+    prize_money = 0
+    min_reward = 0
+    lifeline_used = False
+    while(ques_cnt < 15):
+        print(f'\tQuestion {ques_cnt + 1}: {QUESTIONS[ques_cnt]["name"]}'.format(ques_cnt) )
+        print(f'\t\tOptions:')
+        print(f'\t\t\tOption 1: {QUESTIONS[ques_cnt]["option1"]}')
+        print(f'\t\t\tOption 2: {QUESTIONS[ques_cnt]["option2"]}')
+        print(f'\t\t\tOption 3: {QUESTIONS[ques_cnt]["option3"]}')
+        print(f'\t\t\tOption 4: {QUESTIONS[ques_cnt]["option4"]}')
+        ans = input('Your choice ( 1-4 ) : ')
+        if ans.lower() == "quit":
+            print("Prize Won Till Now: ", prize_money)
+            break
 
-    print(f'\tQuestion 1: {QUESTIONS[0]["name"]}' )
-    print(f'\t\tOptions:')
-    print(f'\t\t\tOption 1: {QUESTIONS[0]["option1"]}')
-    print(f'\t\t\tOption 2: {QUESTIONS[0]["option2"]}')
-    print(f'\t\t\tOption 3: {QUESTIONS[0]["option3"]}')
-    print(f'\t\t\tOption 4: {QUESTIONS[0]["option4"]}')
-    ans = input('Your choice ( 1-4 ) : ')
+        if ans.lower() == "lifeline" and ques_cnt != 14 and not lifeline_used:
+            lifeline_used = True
+            questions = lifeLine(QUESTIONS[ques_cnt])
+            print(f'\tQuestion {ques_cnt + 1}: {questions["name"]}'.format(ques_cnt) )
+            print(f'\t\tOptions:')
+            print(f'\t\t\tOption 1: {questions["option1"]}')
+            print(f'\t\t\tOption 2: {questions["option2"]}')
+            print(f'\t\t\tOption 3: {questions["option3"]}')
+            print(f'\t\t\tOption 4: {questions["option4"]}')
+            ans = input('Your choice ( 1-4 ) : ')
+        
+        # input validation
+        try:
+            int(ans)
+        except:
+            print("Invalid Input, Try Again !!")
+            continue
 
-    # check for the input validations
+        if isAnswerCorrect(QUESTIONS[0], int(ans) ):
+            # print the total money won.
+            # See if the user has crossed a level, print that if yes
+            if ques_cnt == 4:
+                min_reward = "10,000"
+            elif ques_cnt == 10:
+                min_reward == "3,20,000"
+            print('\nCorrect !')
+            prize_money += QUESTIONS[ques_cnt]["money"]
+            print("Prize Won: ", str(prize_money))
 
-    if isAnswerCorrect(QUESTIONS[0], int(ans) ):
-        # print the total money won.
-        # See if the user has crossed a level, print that if yes
-        print('\nCorrect !')
-
-    else:
-        # end the game now.
-        # also print the correct answer
-        print('\nIncorrect !')
-
-    # print the total money won in the end.
+        else:
+            # end the game now.
+            # also print the correct answer
+            prize_money = min_reward
+            print('\nIncorrect !')
+            print("Prize Won: ", str(prize_money))
+            break
+        ques_cnt += 1
+        # print the total money won in the end.
 
 
 kbc()
