640bab88753e6bf74399eae6a0a3631c76173170
Rishabh-Pagaria <rpagaria2000@gmail.com>
2021-06-12 23:30:53 +0530

    initial commit

---
+++ Devsnest Frontend/Day 5/script.js
@@ -0,0 +1,31 @@
+// const input = document.querySelector('input');
+// const numKeys = document.querySelectorAll('.num-key');
+// const opKeys = document.querySelectorAll('.op-key');
+// let buffer = [];
+
+// numKeys.forEach(num => {
+//     num.addEventListener('click',(e)=>{
+//         const numClick = e.target;
+//         input.value = input.value != 0 ? input.value + numClick.innerText : numClick.innerText;
+//         let inputValue =
+//         buffer.push(input.value);
+        
+//     })
+// })
+// console.log(buffer);
+// opKeys.forEach(opp => opp.addEventListener('click',e =>{
+//     const keys = e.target;
+//     const action = keys.dataset.action;
+//     console.log(action);
+    
+// }))
+
+// numKeys.forEach(num => {
+//     num.addEventListener('click', ()=> input.value = input.value != 0 ? input.value + num.innerText : num.innerText);
+// })
+// console.log(input.value);
+// const compute = ()=>{
+    
+// }
+
+// opKeys.forEach(opp => opp.addEventListener('click',compute));
